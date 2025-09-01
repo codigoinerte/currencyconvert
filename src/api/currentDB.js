4da2a752766481374d3e565874dbd4a3eb7aabb2
codigoinerte <fmartinez.bpe@gmail.com>
2025-09-01 14:31:12 -0500

    actualizar la configuración de API para currencyfreaks y corregir la URL de apilayer

--- src/api/currentDB.js
+++ src/api/currentDB.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
 const keyConvert = import.meta.env.VITE_CONVERT_DB_KEY;
-
+const keyCurrencyFreaks =  import.meta.env.VITE_CURRENCY_FREAKS;
 const currentDB = axios.create({
-    baseURL : 'https://api.currencyfreaks.com/',    
+    baseURL : 'https://api.currencyfreaks.com/',  
+    params:{
+        apikey: keyCurrencyFreaks
+    }  
 });
 
 export const convertDB = axios.create({
-    baseURL:'https://api.apilayer.com/currency_data/',
+    baseURL:'https://api.apilayer.com/exchangerates_data/',
     params: {
         apikey : keyConvert
     }
